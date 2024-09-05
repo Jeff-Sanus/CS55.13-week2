87560063684945696bffbfaaf798eda2edec3e10
Jeffrey Rubio <rubiojeff07@gmail.com>
2024-09-04 22:02:14 -0700

    new change

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
 const http = require("http");
-
 const fs = require("fs").promises;
 
-const requestListener = function(req,res) {
+const requestListener = function (req, res) {
     console.log(req.url);
 
     if (req.url === "/") {
-      fs.readFile( __dirname + "/page.html")
-      .then(
-        contents => {
-            res.setHeader("Content-Type", "text/html; charset=UTF-8");
-
-            res.writeHead(200);
-
-            res.end(contents);
-        }
-      )
-
+        // Serve the HTML page
+        fs.readFile(__dirname + "/page.html")
+            .then((contents) => {
+                res.setHeader("Content-Type", "text/html; charset=UTF-8");
+                res.writeHead(200);
+                res.end(contents);
+            })
+            .catch((err) => {
+                res.writeHead(500);
+                res.end("Error loading HTML page");
+                console.error(err);
+            });
+    } else if (req.url === "/data.json") {
+        // Serve the JSON file
+        fs.readFile(__dirname + "/data.json")
+            .then((contents) => {
+                res.setHeader("Content-Type", "application/json; charset=UTF-8");
+                res.writeHead(200);
+                res.end(contents);
+            })
+            .catch((err) => {
+                res.writeHead(500);
+                res.end("Error loading JSON data");
+                console.error(err);
+            });
     } else {
+        // Serve JSON data for any other request
+        const data = [
+            {
+                firstname: "Fred",
+                link: "https://www.santarosa.edu",
+            },
+            {
+                firstname: "Jane",
+                link: "https://www.apple.com",
+            },
+            {
+                firstname: "Mary",
+                link: "https://www.w3schools.com",
+            },
+        ];
 
-        fs.readFile( __dirname + "data.json")
-         .then(
-            contents => {
-            res.setHeader("Content-Type", "application/json; charset=UTF-8");
-            res.writeHead(200);
-            res.end(contents);
-
-          });
-
+        res.setHeader("Content-Type", "application/json; charset=UTF-8");
+        res.writeHead(200);
+        res.end(JSON.stringify(data));
     }
-
 };
 
 const server = http.createServer(requestListener);
 
 const host = "127.0.0.1";
-const port = "3000";
+const port = 3000;
 
-server.listen(
-    port,
-    host,
-    () => {
-        console.log('Server is running')
-
-    }
-);
-
-
-const server = http.createServer(requestListener);
-
-const host = "127.0.0.1";
-const port = "3000";
-
-server.listen(
-    port,
-    host,
-    () => {
-        console.log('Server is running')
-
-    }
-);
+server.listen(port, host, () => {
+    console.log(`Server is running at http://${host}:${port}`);
+});
